3b6e644f46ab44029095c6c04ab71e992c8880ae
Edwin Miyatake <edmiyatake@gmail.com>
2024-11-20 18:58:36 -0800

    trying to build a client server connection to talk to each other

--- FileClient.java
+++ FileClient.java
@@ -1,3 +1,22 @@
+import java.io.*;
+import java.net.*;
+
 public class FileClient {
-    
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost",9090)){
+        socket.setSoTimeout(15000);
+        OutputStream out = socket.getOutputStream();
+        Writer writer = new OutputStreamWriter(out,"UTF-8");
+        writer = new BufferedWriter(writer);
+
+        // InputStream in = socket.getInputStream();
+        // BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        // some  logic here
+        writer.write("Bye Bye");
+        writer.flush(); // flush the buffer
+    }
+    catch (IOException e){
+        System.err.println(e.toString());
+    }
+    }
 }
--- FileServer.java
+++ FileServer.java
@@ -1,3 +1,25 @@
-public class FileServer {
+import java.io.*;
+import java.net.*;
 
+public class FileServer {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        ServerSocket server = new ServerSocket(9090);
+        try (Socket connection = server.accept()) {
+        OutputStream out = connection.getOutputStream();
+        Writer writer = new OutputStreamWriter(out,"ASCII");
+        writer.write("Hello Client!");
+        writer.flush();
+    } 
+    catch (IOException e) {
+        System.err.println(e.toString());
+    } 
+    finally {
+        try {
+            server.close();
+        }
+        catch (IOException e) {
+            System.err.println(e.toString());
+        } 
+    }
+    }
 }
