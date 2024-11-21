6a3978e63c87e513f46279914dcc94f9e96a8f97
Edwin Miyatake <edmiyatake@gmail.com>
2024-11-21 01:50:16 -0800

    successfully implemented a multi threaded server

--- FileServer.java
+++ FileServer.java
@@ -3,35 +3,73 @@ import java.net.*;
 
 public class FileServer {
     public static void main(String[] args) throws UnknownHostException, IOException {
+        
+        // check for valid command line 
+        if (args.length != 1) {
+            System.out.println("Invalid command: Please enter in the format");
+            System.out.println("java Server <directoryPath>");
+            return;
+        }
+
+        // check if file exists and is inside directory
+        File directory = new File(args[0]);
+        if (!directory.exists() || !directory.isDirectory()) {
+            System.out.println("Invalid directory!");
+            return;
+        }
+
         // create a socket that is looking for incoming connections
-        ServerSocket server = new ServerSocket(9090);
-
-        // attempt to connect with the client
-        try (Socket connection = server.accept()) {
-
-            // writing to the client
-            PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
-            out.println("Hello client");
-
-            // reading from the client
-            InputStreamReader read = new InputStreamReader(connection.getInputStream());
-            BufferedReader in = new BufferedReader(read);
-            String clientResponse = in.readLine();
-            System.out.println(clientResponse);
-
-            // close all connections
-            connection.close();
-        } 
-        catch (IOException e) {
-            System.err.println(e.toString());
-        } 
-        finally {
-            try {
-                server.close();
+        try (ServerSocket server = new ServerSocket(9090)){
+        System.out.println("Server has begun looking for client...");
+            while (true) {
+                try {
+                    // attempt to connect with the client
+                    Socket connection = server.accept();
+                    Thread task1 = new FileThread(connection);
+                    task1.start();
+                } 
+                catch (IOException e) {
+                    System.err.println(e.toString());
+                }
+                
             }
+        }
+    }
+
+    private static class FileThread extends Thread {
+        private Socket connection;
+
+        FileThread(Socket connection){
+            this.connection = connection;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // writing to the client
+                PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
+                out.println("Successful connection!");
+
+                // reading from the client
+                InputStreamReader read = new InputStreamReader(connection.getInputStream());
+                BufferedReader in = new BufferedReader(read);
+                String clientResponse = in.readLine();
+                System.out.println(clientResponse);
+
+                // close all connections
+                connection.close();
+            } 
             catch (IOException e) {
                 System.err.println(e.toString());
             } 
+            finally {
+                try {
+                    connection.close();
+                }
+                catch (IOException e) {
+                    System.err.println(e.toString());
+                } 
+            }
         }
     }
 }
